4b64537250750e4a6412a1ece10c9dabaaaeec0e
Adrian Holman <adriholman@hotmail.com>
2022-02-08 10:19:27 -0300

    db try number idk

--- Data/DBConnection.cs
+++ Data/DBConnection.cs
@@ -5,7 +5,7 @@ namespace DigitalNetWeb.Data
     public class DBConnection
     {
         public DataTable? dataTable = new DataTable();
-        string source = "/../App_Data/data.db;Version=3;";
+        string source = "data.db;Version=3;";
 
         public DBConnection()
         {
